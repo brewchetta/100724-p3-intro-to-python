60824d1f3c7a8d9d979319ed5846176846a01965
brewchetta <chett.tiller@gmail.com>
2024-07-11 09:42:28 -0400

    add js for comparison

---
+++ index.js
@@ -0,0 +1,36 @@
+// DECLARING VARIABLES
+
+let firstName = "Chett"
+const lastName = "Tiller"
+var fullName = `${firstName} ${lastName}`
+
+
+// PRIMITIVE DATA TYPES
+
+const someString = "I am a string"
+const someNumber = 12
+const someBoolean = true
+
+
+// OTHER DATA TYPES
+
+const someArray = [1,2,3,4,5]
+const someObject = { name: "Chett", age: "unknown" }
+const someTuple = 'what the heck is a tuple?'
+const someSet = Set( [1,2,3,3,3,3,3,3,3,4,5] )
+
+
+// LOOPS
+
+for (let i = 0; i < someArray.length; i++) {
+    console.log( someArray[i] )
+}
+
+
+// DECLARING FUNCTIONS
+
+function someFunction(arg) {
+    return `This is the argument you put in: ${arg}`
+}
+
+const additionArrowFunc = (x, y) => x + y
